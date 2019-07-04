d6f34e8dbe49c31a2ddb44f78fa9fb99a289d9d7
Alex Judge <alexander.judge@sydney.edu.au>
2019-07-04 15:49:38 +1000

    Amended license

--- utils/utils/utils.py
+++ utils/utils/utils.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Alex Judge
+# Copyright 2019 The University of Sydney
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
